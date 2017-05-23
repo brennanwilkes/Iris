8d81f75e8d7d6e0ba7b0a6dd89a907cbd88de18b
Brennan Wilkes <brennan@codexwilkes.com>
2017-05-22 20:42:05 -0700

    Bug fixes

--- src/ourloader.cc
+++ src/ourloader.cc
@@ -18,34 +18,34 @@ void OurLoader::load_level(Level L,WindowFramework* w,PandaFramework* pf){
 	it needs x,y,z,file,icon,amo,id
 	
 	*/
-	/*
+	
 	for (auto i:L.models){
 		
 		//	i.second	=	NodePath
 		
 		if (i.second.get_tag("class") == "weapon"){
-			WeaponObject* weapon = new WeaponObject('g',i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),i.second.get_tag("wei"),i.second.get_tag("vol"), mydir+i.second.get_tag("file"),objs,w,pf,1.0f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),i.second.get_tag("amo"),i.second.get_tag("id"));
-			weapon->weapon_init(i.second.get_tag("amo"),i.second.get_tag("rate"),1.0,0,i.second.get_tag("ammo"),1);
+			WeaponObject* weapon = new WeaponObject('g',stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),stof(i.second.get_tag("wei")),stof(i.second.get_tag("vol")), mydir+i.second.get_tag("file"),&gameModels,w,pf,1.0f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),stoi(i.second.get_tag("amo")),stoi(i.second.get_tag("id")));
+			weapon->weapon_init(stoi(i.second.get_tag("amo")),stof(i.second.get_tag("rate")),1.0,0,stoi(i.second.get_tag("ammo")),1);
 			itms.push_back(weapon);
 		}
 		else if (i.second.get_tag("class") == "food_item"){
-			FoodItem* FOOD = new FoodItem('c',i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),i.second.get_tag("wei"),i.second.get_tag("vol"), mydir+i.second.get_tag("file"),objs,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),i.second.get_tag("amo"),i.second.get_tag("id"));
+			FoodItem* FOOD = new FoodItem('c',stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),stof(i.second.get_tag("wei")),stof(i.second.get_tag("vol")), mydir+i.second.get_tag("file"),&gameModels,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),stoi(i.second.get_tag("amo")),stoi(i.second.get_tag("id")));
 			itms.push_back(FOOD);
 		}
 		else if (i.second.get_tag("class") == "health_item"){
-			HealthItem* DRUGS = new FoodItem('c',i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),i.second.get_tag("wei"),i.second.get_tag("vol"), mydir+i.second.get_tag("file"),objs,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),i.second.get_tag("amo"),i.second.get_tag("id"));
+			HealthItem* DRUGS = new HealthItem('c',stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),stof(i.second.get_tag("wei")),stof(i.second.get_tag("vol")), mydir+i.second.get_tag("file"),&gameModels,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),stoi(i.second.get_tag("amo")),stoi(i.second.get_tag("id")));
 			itms.push_back(DRUGS);
 		}
 		else if (i.second.get_tag("class") == "water_item"){
-			WaterItem* BOOZE = new FoodItem('c',i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),i.second.get_tag("wei"),i.second.get_tag("vol"), mydir+i.second.get_tag("file"),objs,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),i.second.get_tag("amo"),i.second.get_tag("id"));
+			WaterItem* BOOZE = new WaterItem('c',stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),stof(i.second.get_tag("wei")),stof(i.second.get_tag("vol")), mydir+i.second.get_tag("file"),&gameModels,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),stoi(i.second.get_tag("amo")),stoi(i.second.get_tag("id")));
 			itms.push_back(BOOZE);
 		}
 		else if (i.second.get_tag("class") == "static"){
-			StaticObject* wall = new StaticObject(i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),mydir+i.second.get_tag("file"),&gameModels,window,pf,i.second.get_tag("h"),i.second.get_tag("p"),i.second.get_tag("r"),1.0);
+			StaticObject* wall = new StaticObject(stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),mydir+i.second.get_tag("file"),&gameModels,window,pf,stoi(i.second.get_tag("h")),stoi(i.second.get_tag("p")),stoi(i.second.get_tag("r")),1.0);
 			//this may need to be push_backed to a vector idk tbh
 		}
 		else if (i.second.get_tag("class") == "Ammo"){
-				Item* Ammo= new Item('a',i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),1.0f,1.0f, mydir+i.second.get_tag("file"),objs,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),i.second.get_tag("amo"),i.second.get_tag("id"));
+				Item* Ammo= new Item('a',stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),1.0f,1.0f, mydir+i.second.get_tag("file"),&gameModels,w,pf,0.5f,1,0,0,1.5f,0,mydir+i.second.get_tag("icon"),stoi(i.second.get_tag("amo")),stoi(i.second.get_tag("id")));
 				itms.push_back(Ammo);
 		}
 		else if (i.second.get_tag("class") == "Enemy"){
@@ -55,11 +55,11 @@ void OurLoader::load_level(Level L,WindowFramework* w,PandaFramework* pf){
 			
 			//void Enemy::set_up(NodePath* parent,WindowFramework* w,PandaFramework* pf,string fn,float HEALTH,int xx,int yy,int zz,float dmg,int ifc,int mfc,int afc,float xpe){
 			
-			romar->set_up(&gameModels, w, pf, mydir+i.second.get_tag("file"),i.second.get_tag("heal"),i.second.get_tag("x"),i.second.get_tag("y"),i.second.get_tag("z"),i.second.get_tag("dmg"),40,24,0,i.second.get_tag("xp"));
+			romar->set_up(&gameModels, w, pf, mydir+i.second.get_tag("file"),stof(i.second.get_tag("heal")),stoi(i.second.get_tag("x")),stoi(i.second.get_tag("y")),stoi(i.second.get_tag("z")),stof(i.second.get_tag("dmg")),40,24,0,stof(i.second.get_tag("xp")));
 			romar->init();
 			romar->coll_set_up(1000);
 			enems.push_back(romar);
 		}
 	}
-	*/
+	
 }
